966e9305f6deb2f8b7e8c728119ea7132c17ebcc
Arendelle <Arendelle_@outlook.com>
2025-11-25 10:21:23 +0800

    Small improvment

--- src/services/pltxt2htm/deallocator.ts
+++ src/services/pltxt2htm/deallocator.ts
@@ -3,8 +3,8 @@ import { getWasmInstance } from "./wasmLoader";
 let deallocate: ((ptr: number) => void) | null = null;
 
 export async function getDeallocator() {
-  const wasmInstance = await getWasmInstance();
   if (!deallocate) {
+    const wasmInstance = await getWasmInstance();
     deallocate = wasmInstance.cwrap("deallocate", null, ["number"]);
   }
   return deallocate;
