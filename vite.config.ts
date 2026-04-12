b2fe6043c3fbfe5d779040932d790efe6dd50977
gushishang <117088703+gushishang@users.noreply.github.com>
2026-04-12 18:25:30 +0800

    build fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: import.meta.env.VITE_BASE_URL || "/",
+  base: "/plweb2/",
   plugins: [
     vue(),
     VitePWA({
